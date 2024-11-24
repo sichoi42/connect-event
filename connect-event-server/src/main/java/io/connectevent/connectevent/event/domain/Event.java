df5e393e94c3dbc7edec28437d3ea418247968c9
sichoi42 <42.4.sichoi@gmail.com>
2024-11-24 18:19:28 +0900

    feat: mv jakarta -> javax

--- connect-event-server/src/main/java/io/connectevent/connectevent/event/domain/Event.java
+++ connect-event-server/src/main/java/io/connectevent/connectevent/event/domain/Event.java
@@ -1,15 +1,15 @@
 package io.connectevent.connectevent.event.domain;
 
 import io.connectevent.connectevent.location.domain.Location;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Set;
 import lombok.Getter;
--- connect-event-server/src/main/java/io/connectevent/connectevent/tag/domain/Tag.java
+++ connect-event-server/src/main/java/io/connectevent/connectevent/tag/domain/Tag.java
@@ -1,13 +1,13 @@
 package io.connectevent.connectevent.tag.domain;
 
 import io.connectevent.connectevent.event.domain.EventTag;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
